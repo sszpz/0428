5d67eb8a8654d9e172ab8b2ece8a36389939f5ee
Apratimb <ab6462@nyu.edu>
2019-04-28 11:52:48 -0400

    Intial Heroku

--- YuanYuan/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-const port = process.env.port || 8000
-
-app.get('/asd', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
