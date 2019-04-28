3259b09d3972ae6aea73b24c0b7d151554bfcef7
Apratimb <ab6462@nyu.edu>
2019-04-28 12:07:16 -0400

    Intial Heroku

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port);
